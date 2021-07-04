5e8aa02893e1add6371516172cdcba1d655685a7
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-07-04 17:57:40 +0300

    practices and hws

---
+++ src/Java101/section4/hw2/Main.java
@@ -0,0 +1,33 @@
+package Java101.section4.hw2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        int year, kalan;
+        String burc="";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğum yılınızı giriniz: ");
+        year = input.nextInt();
+
+        kalan = year %12;
+
+        switch (kalan) {
+            case 0 -> burc = "Maymun";
+            case 1 -> burc = "Horoz";
+            case 2 -> burc = "Köpek";
+            case 3 -> burc = "Domuz";
+            case 4 -> burc = "Fare";
+            case 5 -> burc = "Öküz";
+            case 6 -> burc = "Kaplan";
+            case 7 -> burc = "Tavşan";
+            case 8 -> burc = "Ejderha";
+            case 9 -> burc = "Yılan";
+            case 10 -> burc = "At";
+            case 11 -> burc = "Koyun";
+            default -> System.out.print("Hata");
+        }
+
+        System.out.println("Burcunuz: " + burc);
+    }
+}
