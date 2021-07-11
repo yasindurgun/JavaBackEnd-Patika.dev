c7c7230099107b127885eec805bdc80840ea14d4
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-07-11 19:07:10 +0300

    practices and hws

---
+++ src/Java101/section8/hw1/Main.java
@@ -0,0 +1,8 @@
+package Java101.section8.hw1;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Yasin",2000.0,45,"1985");
+        employee.showInfo();
+    }
+}
