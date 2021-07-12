f4a707a1703659a92cfed899fce0c33ebe9e135c
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-07-12 15:59:05 +0300

    practices and hws

---
+++ src/Java101/section9/hw3/Main.java
@@ -0,0 +1,16 @@
+package Java101.section9.hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matris = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int satır = matris.length;
+        int sutun = matris[0].length;
+
+        for(int i=0; i<sutun; i++){
+            for(int j=0; j<satır; j++){
+                System.out.print(matris[j][i]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
